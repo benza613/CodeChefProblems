78ed6b66103fddd0e2ea6b33566e6ad3c3d83450
benza <benito.alvares@gmail.com>
2017-08-05 00:21:22 +0530

    started

--- CodeChefProblems/Program.cs
+++ CodeChefProblems/Program.cs
@@ -10,6 +10,23 @@ namespace CodeChefProblems
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] a = new int[n][];
+            for (int a_i = 0; a_i < n; a_i++)
+            {
+                string[] a_temp = Console.ReadLine().Split(' ');
+                a[a_i] = Array.ConvertAll(a_temp, Int32.Parse);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int numToRemove = a[i].Length-1;
+                int numIndex = Array.IndexOf(a[i], numToRemove);
+                a[i] = a[i].Where((val, idx) => idx != numIndex).ToArray();
+
+                Console.WriteLine(a[i].Max());
+            }
         }
     }
 }
